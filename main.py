c5bd5b8622d0e7b1bb6601eea8317e5393d19491
Michael Bui <58403578+Crystallized21@users.noreply.github.com>
2024-03-19 00:05:51 +1300

    Final Test and Check, CI implemented and learnt!

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import platform
-
-def main():
-    if platform.system() == 'Windows':
-        raise Exception("This script is intentionally failing on Windows.")
-    else:
-        print("This script works on", platform.system())
-
-if __name__ == "__main__":
-    main()
+print("Hello World!")
