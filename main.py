7a4e1bd78b7e20630b928717c60bef1f8e2a91d0
Michael Bui <58403578+Crystallized21@users.noreply.github.com>
2024-03-20 14:57:09 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hellow World!")
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# Calling the factorial function with a negative number
+result = factorial(-5)
+print(result)
